e8ee24d66ba2e5ff9602e5fd2789cf485f4ee4c6
fefifofum <jorsellis@gmail.com>
2014-09-11 03:30:26 +0200

    Fix CPU_VARIANT and add flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,8 +38,12 @@ TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_SMP := true
-TARGET_CPU_VARIANT := cortex-a7
+TARGET_CPU_VARIANT := krait
 ARCH_ARM_HAVE_TLS_REGISTER := true
+
+# Flags
+TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
 
 # Kernel
 #TARGET_KERNEL_SOURCE := kernel/xiaomi/armani
