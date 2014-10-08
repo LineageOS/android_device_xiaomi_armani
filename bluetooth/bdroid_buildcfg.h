8bf93cc76735f71937ea9fc526174d1aebd0216b
fefifofum <jorsellis@gmail.com>
2014-10-08 02:06:07 +0200

    Change bluetooth default name

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -18,7 +18,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "Redmi 1S"
+#define BTM_DEF_LOCAL_NAME   "Xiaomi HM 1S"
 #define BTA_DISABLE_DELAY 1000 /* in milliseconds */
 #define BLUETOOTH_QCOM_SW TRUE
 #define BLUETOOTH_QCOM_LE_INTL_SCAN TRUE
