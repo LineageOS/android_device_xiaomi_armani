567965ba5a88619a09d9934714ef8a10ba76e371
fefifofum <jorsellis@gmail.com>
2015-01-11 16:32:31 +0100

    Enable hardware tunables: vibrator

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -103,6 +103,9 @@ MAX_EGL_CACHE_KEY_SIZE := 12*1024
 # of the device.
 MAX_EGL_CACHE_SIZE := 2048*1024
 
+# Hardware tunables
+BOARD_HARDWARE_CLASS := device/xiaomi/armani/cmhw/
+
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
 
