2aa92ffcadfc139f507c81be991ffa27a9317360
fefifofum <jorsellis@gmail.com>
2015-01-11 13:20:05 +0100

    We have enough space to hold precise GC data

--- device.mk
+++ device.mk
@@ -21,6 +21,8 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
+PRODUCT_TAGS += dalvik.gc.type-precise
+
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 
 # ANT+
