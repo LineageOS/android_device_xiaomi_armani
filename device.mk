f52e96bed48fffdc2445d9c52c1c9864528cdb4d
fefifofum <jorsellis@gmail.com>
2015-02-17 23:01:53 +0100

    Remove languages-full.mk
    * languages_full.mk is inherited by locales_full.mk which is inherited by full_base.mk

--- device.mk
+++ device.mk
@@ -14,8 +14,6 @@
 # limitations under the License.
 #
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
