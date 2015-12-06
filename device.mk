c7c6a8e665d3587e7523c5715730b7b288f8eb13
fefifofum <jorsellis@gmail.com>
2015-12-06 23:10:18 +0100

    Cleanup deprecated media

--- device.mk
+++ device.mk
@@ -148,20 +148,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
 PRODUCT_PACKAGES += \
-    libdashplayer \
-    libmm-omxcore \
-    libOmxAacEnc \
-    libOmxAmrEnc \
     libOmxCore \
-    libOmxEvrcEnc \
-    libOmxQcelp13Enc \
     libOmxVdec \
     libOmxVenc \
-    libstagefrighthw \
-    qcmediaplayer
-
-PRODUCT_BOOT_JARS += \
-    qcmediaplayer
+    libstagefrighthw
 
 # Permissions
 PRODUCT_COPY_FILES += \
