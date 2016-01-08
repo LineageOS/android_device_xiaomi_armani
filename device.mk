d932ba7ef2317cfbf6760cf0448b2397fd7bcdbf
fefifofum <jorsellis@gmail.com>
2016-01-08 15:47:58 +0100

    init.armani.rc: Updates from caf

--- device.mk
+++ device.mk
@@ -212,7 +212,6 @@ PRODUCT_PACKAGES += \
     wcnss_service
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    wifi.interface=wlan0 \
     ro.disableWifiApFirmwareReload=true \
     persist.debug.wfd.enable=1 \
     persist.sys.wfd.virtual=0
