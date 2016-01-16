7777737df495321c0b332e73424fdc7f2364c7b7
fefifofum <jorsellis@gmail.com>
2016-01-16 18:57:45 +0100

    Update init_armani
    The kernel sets ro.boot.modem for both variants of Xiaomi Redmi 1S
    (WCDMA and CDMA), so use that instead of reading the id from a file.
    
    https://github.com/MiCode/Xiaomi_Kernel_OpenSource/commit/9a7ccf68aca38ec52169d0ae60bf623c25c7101e#diff-92d422dd3e36d7f9117a985509729602R40

--- init/init_armani.cpp
+++ init/init_armani.cpp
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2015, The Linux Foundation. All rights reserved.
+   Copyright (c) 2013, The Linux Foundation. All rights reserved.
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
@@ -28,8 +28,6 @@
  */
 
 #include <stdlib.h>
-#include <stdio.h>
-#include <fcntl.h>
 
 #include "vendor_init.h"
 #include "property_service.h"
@@ -38,38 +36,11 @@
 
 #include "init_msm.h"
 
-#define RAW_ID_PATH     "/sys/devices/system/soc/soc0/raw_id"
-#define BUF_SIZE         64
-static char tmp[BUF_SIZE];
-
-static int read_file2(const char *fname, char *data, int max_size)
-{
-    int fd, rc;
-
-    if (max_size < 1)
-        return 0;
-
-    fd = open(fname, O_RDONLY);
-    if (fd < 0) {
-        ERROR("failed to open '%s'\n", fname);
-        return 0;
-    }
-
-    rc = read(fd, data, max_size - 1);
-    if ((rc > 0) && (rc < max_size))
-        data[rc] = '\0';
-    else
-        data[0] = '\0';
-    close(fd);
-
-    return 1;
-}
-
 void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *board_type)
 {
     char platform[PROP_VALUE_MAX];
+    char modem[PROP_VALUE_MAX];
     int rc;
-    unsigned long raw_id = -1;
 
     UNUSED(msm_id);
     UNUSED(msm_ver);
@@ -79,25 +50,17 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
     if (!rc || !ISMATCH(platform, ANDROID_TARGET))
         return;
 
-    /* get raw ID */
-    rc = read_file2(RAW_ID_PATH, tmp, sizeof(tmp));
-    if (rc) {
-        raw_id = strtoul(tmp, NULL, 0);
-    }
+    property_get("ro.boot.modem", modem);
 
     property_set("ro.product.device", "armani");
     property_set("ro.build.product", "armani");
     property_set("ro.build.description", "armani-user 4.4.4 KTU84P V7.1.1.0.KHCMICK release-keys");
     property_set("ro.build.fingerprint", "Xiaomi/armani/armani:4.4.4/KTU84P/V7.1.1.0.KHCMICK:user/release-keys");
 
-    /* HM 1SW  */
-    if (raw_id==2325) {
+    if (strstr(modem, "HM1AW")) {
         property_set("ro.product.model", "HM 1SW");
         property_set("ro.telephony.default_network", "0,1");
-    }
-
-    /* HM 1SC */
-    else {
+    } else if (strstr(modem, "HM1AC")) {
         property_set("ro.product.model", "HM 1SC");
         property_set("ro.telephony.default_network", "7,1");
     }
