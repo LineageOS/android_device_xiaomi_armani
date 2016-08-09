c6f5dbe7e259d8e9ce88770de3d8dfe9449e7d1a
fefifofum <jorsellis@gmail.com>
2016-08-10 01:14:48 +0200

    Remove healthd extension
    Not needed anymore since it's been included in vendor/cm.
    
    http://review.cyanogenmod.org/#/c/155482/

--- device.mk
+++ device.mk
@@ -72,11 +72,6 @@ PRODUCT_PACKAGES += \
     libxml2 \
     Snap
 
-# Charger
-PRODUCT_PACKAGES += \
-    charger_res_images \
-    libhealthd.armani
-
 # Connectivity
 PRODUCT_PACKAGES += \
     libcnefeatureconfig
