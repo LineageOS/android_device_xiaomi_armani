a1a03987dff644888be371ded55f1af513f3f238
Christopher R. Palmer <crpalmer@gmail.com>
2017-01-28 11:27:36 -0500

    armani: Remove low_latency permission, we aren't low latency
    Using Superpowered Latency Test, I get about 80ms which is slower
    than the required 45ms to be truly low_latency.
    
    Change-Id: I9c82a2bb271b5dccfddabad064b67d26a1f53b0c

--- device.mk
+++ device.mk
@@ -144,7 +144,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Permissions
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:system/etc/permissions/com.dsi.ant.antradio_library.xml \
-    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
